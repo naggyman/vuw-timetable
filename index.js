9df146f079525fc538c711085085fb090467c2cc
Morgan French-Stagg <morganfrenchstagg@Morgans-MacBook-Pro-2.local>
2018-08-19 23:01:44 +1200

    Add error handling and year

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cache = require('apicache');
 let app = express();
 
 const PORT = process.env.PORT || 4000
+const errorMsg = "Unable to find that course, please ensure that you have entered in a valid CRN number";
 
 app.use(cache.middleware('5 minutes'));
 
@@ -13,11 +14,15 @@ app.use(cache.middleware('5 minutes'));
 //17180gi
 //http://localhost:4000/calendar?offering=968,17180
 
-function getCalendarFromCourse(offering) {
+function getCalendarFromCourse(offering, year) {
     return new Promise((resolve, reject) => {
-        cachedRequest({url:'https://www.victoria.ac.nz/_service/courses/2.1/offerings/'+ offering + '?year=2018'}, (error, resp, body) => {
+        cachedRequest({url:'https://www.victoria.ac.nz/_service/courses/2.1/offerings/'+ offering + '?year=' + year}, (error, resp, body) => { 
+        let response = JSON.parse(body);
+        if(error || (response.request_status && !response.request_status.success)){
+            reject(error);
+        }
+        else{
             let events = [];
-            let response = JSON.parse(body);
             response.data[0].timetable.items.forEach((item) => {
                 let endDate = new moment(item.endAt).tz("Pacific/Auckland");
                 for(let i = new moment(item.startAt).tz("Pacific/Auckland"); endDate.isSameOrAfter(i); i.add(1, 'week')){
@@ -46,6 +51,7 @@ function getCalendarFromCourse(offering) {
                 }
             });
             resolve(events);
+        }
         });
     });
 }
@@ -53,7 +59,10 @@ function getCalendarFromCourse(offering) {
 
 
 app.get('/ical', (req, res) => {
+    if(!req.query.offerings || !req.query.year) res.status(404).send(errorMsg);
+
     let offerings = req.query.offerings.split(',');
+    let year = req.query.year;
 
     let promises = [];
     for(let i = 0, len = offerings.length; i < len; i++){
@@ -65,25 +74,32 @@ app.get('/ical', (req, res) => {
         let cal = ical({domain: 'https://victoria.ac.nz', name: 'University Timetable'})
         cal.events(array);
         cal.serve(res);
+    }).catch((error) => {
+        res.status(500).send(errorMsg);
     })
 })
 
 app.get('/calendar', (req, res) => {
+    if(!req.query.offerings || !req.query.year) res.status(404).send(errorMsg);
+
     let offerings = req.query.offerings.split(',');
+    let year = req.query.year;
 
     let promises = [];
     for(let i = 0, len = offerings.length; i < len; i++){
-        promises.push(getCalendarFromCourse(offerings[i]));
+        promises.push(getCalendarFromCourse(offerings[i], year));
     }
 
     Promise.all(promises).then((responses) => {
         let array = [].concat.apply([], responses);
         res.send(array);
-    })
+    }).catch((error) => {
+        res.json({error: errorMsg});
+    });
 })
 
 app.get('/', (req, res) => {
-    res.send("<h3>Timetables!</h3> </br> <p>An iCal link is avaliable at https://vuw-timetable.herokuapp.com/ical?offerings=1,2,3 - make sure to put the CRN number in of the courses that you are taking!</p> </br> <p>Built by <a href='https://morgan.french.net.nz'>Morgan French-Stagg</a></p>");
+    res.send("<h3>Timetables!</h3> </br> <p>An iCal link is avaliable at https://vuw-timetable.herokuapp.com/ical?offerings=1,2,3&year=2018 - make sure to put the CRN number in of the courses that you are taking!</p> </br> <p>Built by <a href='https://morgan.french.net.nz'>Morgan French-Stagg</a></p>");
 })
 
 app.listen(PORT, () => {
